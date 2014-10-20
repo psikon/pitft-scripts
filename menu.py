ea1ac076b28bb958f7dce94011430a296b4be36c
Philipp Sehnert <philipp.sehnert@gmail.com>
2014-10-20 17:14:36 +0200

    build up skeleton

---
+++ menu.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+'''
+Description
+'''
+#@author: Philipp Sehnert
+#@contact: philipp.sehnert[a]gmail.com
+
+# imports
+import sys, os
+
+class menu:
+
+	def __init__():
+		pass
+
+	def __del__():
+		pass
--- pi-gui.py
+++ pi-gui.py
@@ -1,51 +1,44 @@
-#!/usr/bin/python
+import pygame 
+import sys, os, time
+from pygame.locals import * 
+import menu
+import controls
 
-from Tkinter import Frame, Tk, BOTH, Text, Menu, END
-from ttk import Button, Style
-import tkFileDialog 
-from PIL import Image, ImageTk
-import os, sys
+def loadImage(filename, colorkey = None):
+	# load picture
+    image = pygame.image.load(filename)
+    # load picture with or without alpha channel
+    if image.get_alpha() == None:
+        image = image.convert()
+    else:
+        image = image.convert_alpha()
+    # set colorkey
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, pygame.RLEACCEL)
+    # return loaded image
+    return image
 
-class Window(Frame):
-  
-    def __init__(self, parent):
-        Frame.__init__(self, parent)   
-         
-        self.parent = parent
-
-        self.MainUI()
-    
-    def MainUI(self):
-      # init options
-      self.parent.title("Main menu")
-      self.pack(fill = BOTH, expand = 1)
-      # set stylle
-      style = Style()
-      style.configure("TFrame", background = "#333")
-      self.style.theme_use("default")
-  
-      fileButton = Button(self, text = "Select",
-      					command = self.onOpen)
-      fileButton.place(x = 100, y = 100)
-
-    def onOpen(self):
-      
-        ftypes = [('mp3 Files', '*.mp3'), ('All files', '*')]
-        dlg = tkFileDialog.Open(self, filetypes = ftypes)
-        fl = dlg.show()
-        
-        if fl != '':
-            text = self.readFile(fl)
-            self.txt.insert(END, text)
-
-    
+def init():
+    pygame.init()
+    screen = pygame.display.set_mode((320,240))
+    screen.blit(loadImage("images/bg.jpg"), (0, 0))
+    pygame.display.update()
 
 
-def main():
-  
-    root = Tk()
-    root.geometry("320x240+0+0")
-    app = Window(root)
-    root.mainloop()  
+def event_loop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                sys.exit()
 
-sys.exit(main())
+def main(): 
+	init()
+	event_loop()
+
+# Check, if this is the main file 
+if __name__ == '__main__': 
+	sys.exit(main())
