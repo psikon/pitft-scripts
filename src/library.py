c739917eae63d06039a5fc51ed201a850a85c76d
Philipp Sehnert <philipp.sehnert@gmail.com>
2014-11-18 16:10:27 +0100

    imporved documentation

---
+++ src/library.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+'''bibliothek creates a screen for selecting the ebooks from a list by showing 
+the name, cover and supplemental informations on the screen. The books are scrollable 
+with the cursor or pitft touchscreen'''
+
+# python imports
+import sys, os
+import pygame
+import eyeD3
+# pi-gui imports
+from interfaces import Interface
+from id3tag import ID3Tag
+
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+
+class Book(pygame.font.Font):
+    '''class for storing all relevant facts about an audio book'''
+
+    def __init__(self, path, title, artist, album, playtime, position, cover):
+      self.path = path
+      self.title = title
+      self.artist = artist
+      self.album = album
+      self.playtime = playtime
+      self.position = float(position)
+      self.cover = cover
+
+    def getPath(self):
+      return self.path
+
+    def getTitle(self):
+      return self.title
+    
+    def getArtist(self):
+      return self.artist
+
+    def getAlbum(self):
+      return self.getAlbum
+
+    def getPlaytime(self):
+      return self.playtime
+
+    def getPosition(self):
+      return self.position
+
+    def getCover(self):
+      return self.cover
+
+class Library:
+    '''generate a library screen for scolling through the audiobooks'''
+
+    def __init__(self, screen, music_folder, function):
+        # define screen variables
+        self.screen = screen
+        self.scr_width = self.screen.get_rect().width
+        self.scr_height = self.screen.get_rect().height
+        # define music folder
+        self.folder = music_folder
+        # important for framerate
+        self.clock = pygame.time.Clock()
+        # contain all interface methods
+        self.interface = Interface()
+        # declare functions
+        self.function = function
+        # create library
+        self.books = self.create_inventory(self.folder)
+        
+
+    def create_inventory(self, music_folder):
+      books = []
+      # create a list of audio books
+      for audio in os.listdir(music_folder):
+          # select all files with specific ending
+          if audio.endswith(".mp3"):
+            # reconstruct path
+            path = music_folder + os.sep + audio
+            tag = ID3Tag(path)
+            book = Book(path, tag.getTitle(), tag.getArtist(), tag.getAlbum(), 
+              tag.getPlaytime(), 0, "images/unknown.jpg")
+            books.append(book)
+      return books
+
+    def on_click(self, index):
+        # get actual position of mouse click 
+        click_pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
+        # scroll to left
+        if 10 <= click_pos[0] <= 55 and 190 <= click_pos[1] <= 235:
+          index -=1
+          # make negative index to max index to start new round
+          if index == -1:
+            index = len(self.books) -1
+          return index
+        # scroll to right
+        if 65 <= click_pos[0] <= 110 and 190 <= click_pos[1] <= 235:
+          index = index + 1
+          # set max index to 0 for start new round 
+          if index >= len(self.books):
+            index = 0
+          return index
+        # select a book
+        if 120 <= click_pos[0] <= 160 and 190 <= click_pos[1] <= 235:
+          print self.books[index]
+          self.function(self.books[index])
+        return index
+
+    def run(self):
+      '''run method for drawing the screen to dispay'''
+      mainloop = True
+      # set index of list to first item
+      index = 0
+      # use infinity loop for drawing screen
+      while mainloop:
+        # Limit frame speed to 30 FPS
+        self.clock.tick(30)
+        # update display when button is pressed
+        for event in pygame.event.get():
+          # draw interface on screen
+          self.interface.list_interface(self.screen, 
+            self.books[index].getTitle(), self.books[index].getArtist(),
+            self.books[index].getPlaytime(), self.books[index].getCover())
+          # mouse button clicked
+          if event.type == pygame.MOUSEBUTTONDOWN:
+            # get position
+            pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
+            # draw circle for touchscreen feedback
+            pygame.draw.circle(self.screen, YELLOW, pos, 10, 0)
+            # update index in library
+            index = self.on_click(index)
+        # update display
+        pygame.display.flip()
+ 
+            
+            
+
+
+ 
+
